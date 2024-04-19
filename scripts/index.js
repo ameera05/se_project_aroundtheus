0236a8c53e714c21b51e983537c911887703cd76
AmeeraMarzouq <marz.ameera@gmail.com>
2024-04-19 11:47:08 -0400

    pop up open close

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,41 @@ const initialCards = [
   },
 ];
 
+/*Elements*/
+
 const profileEditBtn = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseBtn = document.querySelector("#profile-edit-modal-close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+/*Functions*/
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+/*Event Handler*/
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+/*Event Listeners*/
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-const modalCloseBtn = document.querySelector("#profile-edit-modal-close");
+modalCloseBtn.addEventListener("click", (closePopup);
 
-modalCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
