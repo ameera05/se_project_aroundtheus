78cf9bb4a386fb833fc9822f2c5a39c2f9040a75
AmeeraMarzouq <marz.ameera@gmail.com>
2024-06-03 15:25:03 -0400

    add label

--- scripts/validation.js
+++ scripts/validation.js
@@ -62,7 +62,7 @@ const config = {
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
   inactiveButtonClass: ".modal__button_disabled",
-  inputErrorClass: ".modal__input_type_error",
+  inputErrorClass: "modal__input_type_error",
   errorClass: ".modal__error",
 };
 
