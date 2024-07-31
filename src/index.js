20f929241bf05c38cc198e6ccef6f6fb5d2fde73
AmeeraMarzouq <marz.ameera@gmail.com>
2024-07-31 15:16:15 -0400

    errors

--- src/index.js
+++ src/index.js
@@ -51,6 +51,16 @@ const previewImageLabel = document.querySelector(".modal__image-label");
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+  document.removeEventListener("keydown", handleEscClose);
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+  document.addEventListener("keydown", handleEscClose);
+}
+
 function handleImageClick(cardData) {
   openModal(previewImageModal);
   previewImageElement.src = cardData.link;
@@ -72,6 +82,12 @@ function renderCard(cardData, wrapper) {
 /* -------------------------------------------------------------------------- */
 /*                               Event Handlers                               */
 /* -------------------------------------------------------------------------- */
+//old code
+//function handleProfileEditSubmit(e) {
+//profileTitle.textContent = profileTitleInput.value;
+//profileDescription.textContent = profileDescriptionInput.value;
+//closeModal(profileEditModal);
+//}
 
 function handleProfileEditSubmit(e) {
   profileTitle.textContent = profileTitleInput.value;
@@ -104,7 +120,16 @@ profileEditBtn.addEventListener("click", () => {
   editProfilePopup.open();
 });
 
+//profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+//addCardFormElement.addEventListener("submit", handleProfileAddSubmit);
+//profileEditBtn.addEventListener("click", () => {
+profileTitleInput.value = profileTitle.textContent;
+profileDescriptionInput.value = profileDescription.textContent;
+openModal(profileEditModal);
+//});
+
 // add new card button
+//addNewCardButton.addEventListener("click", () => openModal(profileAddModal));
 
 addNewCardButton.addEventListener("click", () => {
   newCardPopup.open();
